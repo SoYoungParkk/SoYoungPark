35ef3cb5cfe5d00be84164e8e6e1e989ae02f7d0
SoYoungParkk <162673071+SoYoungParkk@users.noreply.github.com>
2024-05-05 13:09:03 +0900

    05052

--- app/main.py
+++ app/main.py
@@ -1,96 +1,16 @@
-"""
-from typing import Union
+from flask import Flask, render_template, request
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.route('/hotplaces', methods=['POST'])
+def hotplaces():
+    if request.method == 'POST':
+        location = request.form['location']
+        return f"입력받은 지역: {location}"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-
-
-"""
-"""
-
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-
-app = FastAPI()
-
-user_name = None
-
-class User(BaseModel):
-    name: str
-
-
-@app.get("/")
-def root():
-    return{ "message": "Hello Bosman!"}
-
-@app.get("/home")
-def home():
-    return { "message": "Bye Bosman!" }
-
-#####여기까지 저번주########
-
-
-@app.post("/user/")
-async def receive_user(user: User):
-    global user_name
-    user_name = user.name
-    return {"message": "User name received"}
-
-
-@app.get("/user/")
-async def get_user():
-    return {"user_name": user_name}
-
-@app.put("/user/")
-async def receive_user(user: User):
-    global user_name
-    user_name = user.name
-    return {"message": "User name changed"}
-
-
-@app.delete("/user/")
-async def del_user():
-    global user_name
-    user_name = "DELETED"
-    return {"message": "User name deleted"}
-
-"""
-
-from fastapi import FastAPI
-
-import requests
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=ergT0Yyea577o2lLp4COXsCi6x0dVh8yvr5bTQ6P&returnType=json"
-
-    contents = requests.get(URL).text
-
-    return {"message": contents}
-
-@app.get("/home")
-def home():
-    return {"message": "Home!"}
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
