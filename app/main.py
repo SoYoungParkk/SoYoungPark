ce16354f364f10581532dddd8e1d976a8ccfc9cc
SoYoungParkk <162673071+SoYoungParkk@users.noreply.github.com>
2024-05-06 13:25:12 +0900

    0506

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
-from fastapi import FastAPI, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+hot_places = {
+    "Seoul": [
+        {"name": "Place 1", "latitude": 37.123, "longitude": 127.456},
+        {"name": "Place 2", "latitude": 37.234, "longitude": 127.567},
+        {"name": "Place 3", "latitude": 37.345, "longitude": 127.678},
+    ],
+    "Busan": [
+        {"name": "Place A", "latitude": 35.234, "longitude": 129.345},
+        {"name": "Place B", "latitude": 35.345, "longitude": 129.456},
+        {"name": "Place C", "latitude": 35.456, "longitude": 129.567},
+    ]
+}
 
-@app.get("/location", response_class=HTMLResponse)
-def get_location_form():
-    form = """
-    <form method="post">
-    <label for="location">지역 입력:</label><br>
-    <input type="text" id="location" name="location" required><br>
-    <button type="submit">확인</button>
-    </form>
-    """
-    return form
-
-@app.post("/location")
-def receive_location(location: str = Form(...)):
-    return {"입력받은 지역": location}
+@app.get("/hot-places/{city}")
+async def get_hot_places(city: str):
+    return hot_places.get(city, [])
